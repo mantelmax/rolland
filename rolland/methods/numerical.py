1ad8c6a4485716910f983dc63380f8d54c148a1c
Benjamin Janek Wagner <b.wagner.2@campus.tu-berlin.de>
2026-07-23 17:26:48 +0200

    rework numerical model stampka for current track and components structure

--- rolland/methods/numerical.py
+++ rolland/methods/numerical.py
@@ -30,10 +30,6 @@ from numpy.fft import fft, fftfreq
 from scipy.sparse import SparseEfficiencyWarning, csc_matrix, diags, eye
 from scipy.sparse.linalg import splu
 
-from rolland.boundary import PMLRailDampVertic
-from rolland.deflection import Deflection
-from rolland.discretization import Discretization
-from rolland.excitation import Excitation
 from rolland.methods import AnalyticalMethods
 from rolland.track import (
     ArrangedBallastedSingleRailTrack,
@@ -68,159 +64,6 @@ class PMLRailDampVertic:
     def pml(self, drbc, xbc):
         """Exponential increasing rail damping, added to dr."""
         return drbc * xbc ** self.alpha / self.l_bound ** self.alpha
-
-#---deflection.py---
-@dataclass(kw_only=True)
-class Deflection(ABC):
-    r"""Abstract base class for deflection classes.
-
-    Attributes
-    ----------
-    excit : Excitation
-        Excitation instance.
-    discr : Discretization
-        Discretization instance.
-    """
-
-    discr: Discretization
-    excit: Excitation
-    track: Track = field(init=False)
-
-    def __post_init__(self, *args, **kwargs):
-        """post_init method to set track attribute after initialization."""
-        self.track = self.discr.track
-
-    @abstractmethod
-    def _abstract(self) -> None:
-        """Prevents instantiation of abstract classes."""
-
-
-class DeflectionEBBVertic(Deflection):
-    r"""Calculate deflection according to :cite:t:`stampka2022a`.
-
-    Attributes
-    ----------
-    track : Track
-        Track instance.
-    excit : Excitation
-        Excitation instance.
-    discr : Discretization
-        Discretization instance.
-    deflection : numpy.ndarray
-        Deflection array :math:`[m]`.
-    ind_excit : int
-        Index of excitation point :math:`[-]`.
-    """
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initialize the DeflectionFDMStampka class.
-
-        Parameters
-        ----------
-        *args : tuple
-            Variable length argument list.
-        **kwargs : dict
-            Arbitrary keyword arguments.
-
-        Attributes
-        ----------
-        deflection : numpy.ndarray
-            Array of calculated deflections with shape (2 * nx, nt + 1).
-        """
-        super().__init__(*args, **kwargs)
-        # Initialize starting values
-        self.calc_force()
-        defl = self.initialize_start_values()
-        # Calculate deflection
-        self.deflection = self.calc_deflection(defl)
-
-    __init__.__signature__ = inspect.signature(Deflection.__init__)
-
-    def initialize_start_values(self):
-        """Set starting values of deflections to zero.
-
-        Returns
-        -------
-        defl : numpy.ndarray
-            Array of deflections initialized to zero with shape (2 * nx, nt + 1).
-        """
-        defl = empty((2 * self.discr.nx, self.discr.nt + 1))
-
-        # Set starting values to zero for two time steps
-        defl[:, 0:2] = zeros((2 * self.discr.nx, 2))
-        return defl
-
-    def calc_force(self):
-        """Calculate force array."""
-        t = linspace(0, self.discr.sim_t, self.discr.nt)
-        self.force = self.excit.force(t)
-
-    def calc_rightside_crank_nicolson(self, u1, u0, ind_excit, t):
-        """Calculate the right-hand side of the equation according to :cite:t:`stampka2022a`.
-
-        Parameters
-        ----------
-        u1 : numpy.ndarray
-            Deflection array at the current time step.
-        u0 : numpy.ndarray
-            Deflection array at the previous time step.
-        ind_excit : int
-            Index of the excitation point.
-        t : int
-            Current time step.
-
-        Returns
-        -------
-        numpy.ndarray
-            Right-hand side of the equation.
-        """
-        # Write excitation force for time step t into force array
-        f = zeros(2 * self.discr.nx)
-
-        if isinstance(ind_excit, list):
-            for idx in ind_excit:
-                f[idx] = self.force[t]
-        else:
-            f[ind_excit] = self.force[t]
-
-        return self.discr.B.dot(u1) + self.discr.C.dot(u0) + self.discr.dt**2 / (self.track.rail.mr * self.discr.dx) * f
-
-    def calc_deflection(self, defl):
-        """
-        Calculate deflection.
-
-        Parameters
-        ----------
-        defl : numpy.ndarray
-            Array of deflections initialized to zero with shape (2 * nx, nt + 1).
-
-        Returns
-        -------
-        defl : numpy.ndarray
-            Array of calculated deflections with shape (2 * nx, nt + 1).
-        """
-        # Index of excitation point/points
-        if isinstance(self.excit.x_excit, list):
-            self.ind_excit = [int(x / self.discr.dx) for x in self.excit.x_excit]
-        else:
-            self.ind_excit = int(self.excit.x_excit / self.discr.dx)
-
-        # Factorization of matrix A (LU decomposition)
-        factoriz = splu(self.discr.A)
-
-        for t in range(1, self.discr.nt):
-            # Calculate right hand side of equation
-            b = self.calc_rightside_crank_nicolson(u1=defl[:, t], u0=defl[:, t - 1], ind_excit=self.ind_excit, t=t)
-
-            # Calculate deflection for time step t
-            u = factoriz.solve(b)
-
-            defl[:, t + 1] = u[0 : 2 * self.discr.nx]
-        return defl
-
-    def _abstract(self) -> None:
-        pass
 
 #---discretization.py---
 class Discretization(ABC):
@@ -522,8 +365,8 @@ class DiscretizationEBBVerticConst(DiscretizationEBBVertic):
         """
         if isinstance(self.track, ContSlabSingleRailTrack):
             # Properties are assigned to each grid point
-            self.vec_sp += self.track.pad.sp[0]
-            self.vec_dp += self.track.pad.dp[0]
+            self.vec_sp += self.track.pad.sp_z
+            self.vec_dp += self.track.pad.dp_z
             self.vec_ms += self.track.slab.ms
 
         elif isinstance(self.track, SimplePeriodicSlabSingleRailTrack | ArrangedSlabSingleRailTrack):
@@ -531,11 +374,11 @@ class DiscretizationEBBVerticConst(DiscretizationEBBVertic):
 
         elif isinstance(self.track, ContBallastedSingleRailTrack):
             # Properties are assigned to each grid point
-            self.vec_sp += self.track.pad.sp[0]
-            self.vec_dp += self.track.pad.dp[0]
+            self.vec_sp += self.track.pad.sp_z
+            self.vec_dp += self.track.pad.dp_z
             self.vec_ms += self.track.slab.ms
-            self.vec_sb += self.track.ballast.sb[0]
-            self.vec_db += self.track.ballast.db[0]
+            self.vec_sb += self.track.ballast.sb_z
+            self.vec_db += self.track.ballast.db_z
 
         elif isinstance(self.track, SimplePeriodicBallastedSingleRailTrack | ArrangedBallastedSingleRailTrack):
             self.build_discrete_ballasted_track()
@@ -562,8 +405,8 @@ class DiscretizationEBBVerticConst(DiscretizationEBBVertic):
         mount_pos = list(self.track.mount_prop.keys())
         for i in mount_pos:
             x_ind = int(i / self.dx)
-            self.vec_sp[x_ind] = self.track.mount_prop[i][0].sp[0] / self.dx
-            self.vec_dp[x_ind] = self.track.mount_prop[i][0].dp[0] / self.dx
+            self.vec_sp[x_ind] = self.track.mount_prop[i][0].sp_z / self.dx
+            self.vec_dp[x_ind] = self.track.mount_prop[i][0].dp_z / self.dx
             self.vec_ms[x_ind] = self.track.slab.ms / self.dx
 
     def build_discrete_ballasted_track(self):
@@ -583,11 +426,11 @@ class DiscretizationEBBVerticConst(DiscretizationEBBVertic):
         mount_pos = list(self.track.mount_prop.keys())
         for i in mount_pos:
             x_ind = int(i / self.dx)
-            self.vec_sp[x_ind] = self.track.mount_prop[i][0].sp[0] / self.dx
-            self.vec_dp[x_ind] = self.track.mount_prop[i][0].dp[0] / self.dx
+            self.vec_sp[x_ind] = self.track.mount_prop[i][0].sp_z / self.dx
+            self.vec_dp[x_ind] = self.track.mount_prop[i][0].dp_z / self.dx
             self.vec_ms[x_ind] = self.track.mount_prop[i][1].ms / self.dx
-            self.vec_sb[x_ind] = self.track.mount_prop[i][2].sb[0] / self.dx
-            self.vec_db[x_ind] = self.track.mount_prop[i][2].db[0] / self.dx
+            self.vec_sb[x_ind] = self.track.mount_prop[i][2].sb_z / self.dx
+            self.vec_db[x_ind] = self.track.mount_prop[i][2].db_z / self.dx
 
 
 class DiscretizationEBBVerticTimeDepend(DiscretizationEBBVerticConst):
@@ -665,6 +508,159 @@ class GaussianImpulse(StationaryExcitation):
         """Compute force array (contains force over time)."""
         tg = t - 4 * self.sigma
         return self.a * tg / self.sigma ** 2 * exp(-tg ** 2 / self.sigma ** 2)
+
+#---deflection.py---
+@dataclass(kw_only=True)
+class Deflection(ABC):
+    r"""Abstract base class for deflection classes.
+
+    Attributes
+    ----------
+    excit : Excitation
+        Excitation instance.
+    discr : Discretization
+        Discretization instance.
+    """
+
+    discr: Discretization
+    excit: Excitation
+    track: Track = field(init=False)
+
+    def __post_init__(self, *args, **kwargs):
+        """post_init method to set track attribute after initialization."""
+        self.track = self.discr.track
+
+    @abstractmethod
+    def _abstract(self) -> None:
+        """Prevents instantiation of abstract classes."""
+
+
+class DeflectionEBBVertic(Deflection):
+    r"""Calculate deflection according to :cite:t:`stampka2022a`.
+
+    Attributes
+    ----------
+    track : Track
+        Track instance.
+    excit : Excitation
+        Excitation instance.
+    discr : Discretization
+        Discretization instance.
+    deflection : numpy.ndarray
+        Deflection array :math:`[m]`.
+    ind_excit : int
+        Index of excitation point :math:`[-]`.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the DeflectionFDMStampka class.
+
+        Parameters
+        ----------
+        *args : tuple
+            Variable length argument list.
+        **kwargs : dict
+            Arbitrary keyword arguments.
+
+        Attributes
+        ----------
+        deflection : numpy.ndarray
+            Array of calculated deflections with shape (2 * nx, nt + 1).
+        """
+        super().__init__(*args, **kwargs)
+        # Initialize starting values
+        self.calc_force()
+        defl = self.initialize_start_values()
+        # Calculate deflection
+        self.deflection = self.calc_deflection(defl)
+
+    __init__.__signature__ = inspect.signature(Deflection.__init__)
+
+    def initialize_start_values(self):
+        """Set starting values of deflections to zero.
+
+        Returns
+        -------
+        defl : numpy.ndarray
+            Array of deflections initialized to zero with shape (2 * nx, nt + 1).
+        """
+        defl = empty((2 * self.discr.nx, self.discr.nt + 1))
+
+        # Set starting values to zero for two time steps
+        defl[:, 0:2] = zeros((2 * self.discr.nx, 2))
+        return defl
+
+    def calc_force(self):
+        """Calculate force array."""
+        t = linspace(0, self.discr.sim_t, self.discr.nt)
+        self.force = self.excit.force(t)
+
+    def calc_rightside_crank_nicolson(self, u1, u0, ind_excit, t):
+        """Calculate the right-hand side of the equation according to :cite:t:`stampka2022a`.
+
+        Parameters
+        ----------
+        u1 : numpy.ndarray
+            Deflection array at the current time step.
+        u0 : numpy.ndarray
+            Deflection array at the previous time step.
+        ind_excit : int
+            Index of the excitation point.
+        t : int
+            Current time step.
+
+        Returns
+        -------
+        numpy.ndarray
+            Right-hand side of the equation.
+        """
+        # Write excitation force for time step t into force array
+        f = zeros(2 * self.discr.nx)
+
+        if isinstance(ind_excit, list):
+            for idx in ind_excit:
+                f[idx] = self.force[t]
+        else:
+            f[ind_excit] = self.force[t]
+
+        return self.discr.B.dot(u1) + self.discr.C.dot(u0) + self.discr.dt**2 / (self.track.rail.mr * self.discr.dx) * f
+
+    def calc_deflection(self, defl):
+        """
+        Calculate deflection.
+
+        Parameters
+        ----------
+        defl : numpy.ndarray
+            Array of deflections initialized to zero with shape (2 * nx, nt + 1).
+
+        Returns
+        -------
+        defl : numpy.ndarray
+            Array of calculated deflections with shape (2 * nx, nt + 1).
+        """
+        # Index of excitation point/points
+        if isinstance(self.excit.x_excit, list):
+            self.ind_excit = [int(x / self.discr.dx) for x in self.excit.x_excit]
+        else:
+            self.ind_excit = int(self.excit.x_excit / self.discr.dx)
+
+        # Factorization of matrix A (LU decomposition)
+        factoriz = splu(self.discr.A)
+
+        for t in range(1, self.discr.nt):
+            # Calculate right hand side of equation
+            b = self.calc_rightside_crank_nicolson(u1=defl[:, t], u0=defl[:, t - 1], ind_excit=self.ind_excit, t=t)
+
+            # Calculate deflection for time step t
+            u = factoriz.solve(b)
+
+            defl[:, t + 1] = u[0 : 2 * self.discr.nx]
+        return defl
+
+    def _abstract(self) -> None:
+        pass
 
 #---postprocessing.py---
 class PostProcessing(ABC):
