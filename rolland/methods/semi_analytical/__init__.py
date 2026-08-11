a314b88a62a64bf14792507a0e322add9cc7312b
max <maximilian.mantel@tu-berlin.de>
2026-08-11 17:27:38 +0200

    refactor: add analytical models for continuous tracks using Kostovasilis approach

--- rolland/methods/semi_analytical/__init__.py
+++ rolland/methods/semi_analytical/__init__.py
@@ -4,9 +4,15 @@ from .tsb_analytical import (
     TSBDiscr1L,
     TSBDiscr2L,
 )
+from .kosto import (
+    TBCont1LKosto,
+    TBCont2LKosto,
+)
 
 __all__ = [
     "TSBDiscr",
     "TSBDiscr1L",
     "TSBDiscr2L",
+    "TBCont1LKosto",
+    "TBCont2LKosto",
 ]
